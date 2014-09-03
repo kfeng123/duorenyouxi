6301990b0bd7c6ece961596a53623026395aef22
kfeng123 <446100240@qq.com>
2014-09-03 20:54:23 +0800

    test

---
+++ index.js
@@ -0,0 +1,28 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+
+http.listen(process.env.PORT||3000, function(){
+  console.log('listening on *:3000');
+});
