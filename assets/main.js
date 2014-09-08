4a94a41ba3d86c34afea8f93ac7fa3cb547b06f8
kfeng123 <446100240@qq.com>
2014-09-08 21:24:20 +0800

    test

--- assets/main.js
+++ assets/main.js
@@ -133,7 +133,7 @@ util.makeGame.prototype={
 		
 		//使用技能
 		if(this.GAME.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
-			this.role.longPaoXiao.reset(this.role.x,this.role.y);
+			//this.role.longPaoXiao.reset(this.role.x,this.role.y);
 			this.role.longPaoXiao.play('do',8,false,true);
 		
 		}
