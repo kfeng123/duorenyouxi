678337cb3cb730eaf20f2b14fab76185e91c4d36
kfeng123 <446100240@qq.com>
2014-09-13 18:50:10 +0800

    test

--- index.js
+++ index.js
@@ -108,7 +108,8 @@ io.on('connection', function(socket){
   
   //定时更新玩家的怪物信息
   emitter.on('updateMonster',function(){
-	socket.emit('updateMonster',JSON.stringify(presentMonster));
+	socket.emit('updateMonster','hehe');
+	//socket.emit('updateMonster',JSON.stringify(presentMonster));
   });
   
   
