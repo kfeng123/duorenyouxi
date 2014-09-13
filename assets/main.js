17dcd14c90fab3970355514fe59619b7b5e7bce9
kfeng123 <446100240@qq.com>
2014-09-13 18:52:01 +0800

    test

--- assets/main.js
+++ assets/main.js
@@ -147,7 +147,7 @@ util.makeGame.prototype={
 		//从服务器获取怪物数据
 		socket.on('updateMonster',function(jstring){
 			alert(jstring);
-			this.EnemyDataFromServer=JSON.parse(jstring);
+			//this.EnemyDataFromServer=JSON.parse(jstring);
 			
 		});
 		
